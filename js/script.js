1a04bb6b703e223d81652783574fe1f9664d41cb
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-15 10:21:33 +0200

    drfg

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const alert = document.querySelector(".alert");
+const input = document.querySelector("#grocery-input");
+const submitBtn = document.querySelector(".submit-btn");
+const ul = document.querySelector(".ul");
+const clearBtn = document.querySelector(".clear-btn");
+
+console.log(clearBtn);
