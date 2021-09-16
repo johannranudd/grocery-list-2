79608a7d4ae20d5386db2b9517e36a33bfb7f791
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-16 09:32:12 +0200

    sdf

--- js/script.js
+++ js/script.js
@@ -3,5 +3,3 @@ const input = document.querySelector("#grocery-input");
 const submitBtn = document.querySelector(".submit-btn");
 const ul = document.querySelector(".ul");
 const clearBtn = document.querySelector(".clear-btn");
-
-console.log(clearBtn);
