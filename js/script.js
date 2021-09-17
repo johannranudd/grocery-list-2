ef6f97a8ac3ffdc3cac19b89232d7f11f7516e07
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-17 10:23:48 +0200

    gdfg

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let editID = "";
 
 //***** EVENT LISTENERS *****
 form.addEventListener("submit", displayItem);
+clearBtn.addEventListener("click", clearItems);
 
 //***** FUNCTIONS *****
 function displayItem(e) {
@@ -23,24 +24,52 @@ function displayItem(e) {
     // create item
     const listItem = document.createElement("li");
     listItem.classList.add("list-item");
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    listItem.setAttributeNode(attr);
+    listItem.innerHTML = `${value}<div class="buttons">
+    <button class="btn edit-btn">
+      <i class="fas fa-edit"></i>
+    </button>
+    <button class="btn delete-btn">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>`;
 
-    console.log(listItem);
-    // add class
-    // item innerHTML
-    // create attr
-    // set attr value
-    // set artNode to item
+    const editBtn = listItem.querySelector(".edit-btn");
+    const deleteBtn = listItem.querySelector(".delete-btn");
+
+    ul.appendChild(listItem);
+    clearBtn.classList.add("display-block");
+    alertFn("success", "Item added");
+    // addToLocalHost(value, id);
+    setBackToDefault();
     // get buttons
     // add eventlistener to btns
-    // append to ul
-    // display alert
-    // add to local host
-    // setBackToDefault
   } else if (value && editFlag) {
     // console.log("edit item");
   } else {
     // console.log("please enter value");
     alertFn("danger", "please enter a value");
+  }
+}
+
+function clearItems() {
+  const listItem = ul.querySelectorAll(".list-item");
+  listItem.forEach(function (item) {
+    item.remove();
+  });
+  alertFn("danger", "List cleared");
+  setBackToDefault();
+}
+
+function setBackToDefault() {
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "Add";
+  input.value = "";
+  if (ul.children.length === 0) {
+    clearBtn.classList.remove("display-block");
   }
 }
 
